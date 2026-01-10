074b05ece828eea3185f1ec01ac92dfe8de24e09
k-sailee <d2023.sailee.khedkar@ves.ac.in>
2026-01-10 14:08:53 +0530

    Added class features

---
+++ frontend/src/services/scheduleService.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:5000/api/scheduler",
+});
+
+export const createClass = async (data: any) => {
+  const res = await API.post("/class", data);
+  return res.data;
+};
+
+export const getTeacherClasses = async (teacherEmail: string) => {
+  const res = await API.get("/teacher/classes", {
+    params: { teacherEmail },
+  });
+  return res.data;
+};
+
+export const getStudentClasses = async (studentId: string) => {
+  const res = await API.get("/student/classes", {
+    params: { studentId },
+  });
+  return res.data;
+};
